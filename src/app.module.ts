8137402138f6fa3b9776677ac57e6a711997d664
gotoinc <root@gotoinc.co>
2023-05-24 22:16:38 +0300

    Connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'task-unity',
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [],
   providers: [],
 })
