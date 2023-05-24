eb9b2f4eb01db338a31baed427aa11777a5e2b6e
gotoinc <root@gotoinc.co>
2023-05-24 22:25:10 +0300

    Create skeleton for auth entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forRoot({
@@ -12,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
     database: 'task-unity',
     autoLoadEntities: true,
     synchronize: true,
-  })],
+  }), AuthModule],
   controllers: [],
   providers: [],
 })
